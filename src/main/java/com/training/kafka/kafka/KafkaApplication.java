86c19ed1d43baa1f5905fba81025b85ddb20dfb1
Carlos Loaeza <carlosloaeza100598@gmail.com>
2025-09-26 18:32:06 -0600

    micrometer

--- src/main/java/com/training/kafka/kafka/KafkaApplication.java
+++ src/main/java/com/training/kafka/kafka/KafkaApplication.java
@@ -6,7 +6,6 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -14,24 +13,31 @@ import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import io.micrometer.core.instrument.Meter;
+import io.micrometer.core.instrument.MeterRegistry;
+
 @SpringBootApplication
-public class KafkaApplication implements CommandLineRunner {
+public class KafkaApplication {
 
 	@Autowired
 	private KafkaTemplate<String, String> kafkaTemplate;
 
+	@Autowired
+	private MeterRegistry meterRegistry;
+
 	private static final Logger log = LoggerFactory.getLogger(KafkaApplication.class);
 
-	@KafkaListener(id = "clog10", autoStartup = "false", topicPartitions = @TopicPartition(topic = "clog10-topic", partitions = {
+	@KafkaListener(id = "clog10", autoStartup = "true", topicPartitions = @TopicPartition(topic = "clog10-topic", partitions = {
 			"0", "1", "2", "3",
 			"4" }), containerFactory = "kafkaListenerContainerFactory", groupId = "clog10-group", properties = {
 					"max.poll.interval.ms:4000",
-					"max.poll.records:10" })
+					"max.poll.records:50" })
 	public void listen(List<ConsumerRecord<String, String>> messages) {
 		log.info("Starting a new batch of messages");
 		for (ConsumerRecord<String, String> message : messages) {
-			log.info("Received Messasge, Partition = {}, Offset = {}, Key={}, Value={}", message.partition(),
-					message.offset(), message.key(), message.value());
+			// log.info("Received Messasge, Partition = {}, Offset = {}, Key={}, Value={}",
+			// message.partition(),
+			// message.offset(), message.key(), message.value());
 		}
 		log.info("Batch of messages completed");
 	}
@@ -40,18 +46,24 @@ public class KafkaApplication implements CommandLineRunner {
 		SpringApplication.run(KafkaApplication.class, args);
 	}
 
-	@Scheduled(fixedDelay = 1000, initialDelay = 500)
-	public void print() {
+	@Scheduled(fixedDelay = 2000, initialDelay = 100)
+	public void sendKafkaMessages() {
 		log.info("clog10 rules");
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < 200; i++) {
 			kafkaTemplate.send("clog10-topic", String.valueOf(i),
 					"Hello World from Spring Kafka! " + i);
 		}
+	}
+
+	@Scheduled(fixedDelay = 2000, initialDelay = 500)
+	public void messageCountMetric() {
+		List<Meter> metrics = meterRegistry.getMeters();
+		for (Meter metric : metrics) {
+			log.info("Meter = {}", metric.getId().getName());
+		}
+
+		double count = meterRegistry.get("kafka.producer.record.send.total").functionCounter().count();
+		log.info("Count {}", count);
 	}
 
 }
